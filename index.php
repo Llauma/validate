ad1a1789b296ec05f0b0121e520c44d744bc3228
Llauma <97829047+Llauma@users.noreply.github.com>
2022-10-30 03:37:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+app = require("express")();
 
+const port = process.env.PORT || 80;
+const users = ["katerina4cat", "Lunya"];
+
+app.get("/validate", (req, res) => {
+    const username = req.query.username || null
+
+    if (username === null) {
+        res.send(false);
+        return;
+    }
+    for (const i in users)
+        if (users[i] === username) {
+            res.send(true);
+            return;
+        }
+
+    res.send(false);
+});
+
+app.listen(port, () => console.log(`Сервер запущен, на порту ${port}`));
